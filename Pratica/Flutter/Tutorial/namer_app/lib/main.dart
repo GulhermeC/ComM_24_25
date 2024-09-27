d7e034623c0aa3bb1c35a232227be80eaf2c5c04
Guilherme Craveiro <gjscraveiro@ua.pt>
2024-09-27 18:19:43 +0100

    flutter tot

---
+++ Pratica/Flutter/Tutorial/namer_app/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App Title'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              //print('Print something');
+            },
+            child: Text('Click Me'),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+            BottomNavigationBarItem(
+                label: 'Settings', icon: Icon(Icons.settings)),
+          ],
+        ),
+      ),
+    );
+  }
+}
